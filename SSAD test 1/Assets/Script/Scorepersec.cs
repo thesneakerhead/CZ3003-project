9f03ebc90e1be78d5c68e4fe0b11435a34eaf0d0
hingyeeneedssleep <hingyeeyuen39@gmail.com>
2021-03-18 14:57:49 +0800

    AI movement + score display

---
+++ SSAD test 1/Assets/Script/Scorepersec.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scorepersec : MonoBehaviour
+{
+    public Text scoreText;
+    public float scoreAmount;
+    public float pointIncreased;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreAmount = 0f;
+        pointIncreased = 1f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        scoreText.text = $"EnemyScore: {(int)scoreAmount}";
+        if (timer > 5f)
+        {
+            scoreAmount += pointIncreased;
+            scoreText.text = scoreText.ToString();
+            timer = 0;
+        }
+       
+          
+        
+    }
+}
